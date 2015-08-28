e6c5330a539f237feea95b482b6895d2dbb6cfe1
Fred Hudson <remotezen@bell.net>
2015-08-28 12:18:23 -0400

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(title = '')
+    filler = "Real Rails Application"
+    bar =" | "
+    if title.empty?
+       filler
+    else
+      title + bar + filler
+    end
+  end
 end
