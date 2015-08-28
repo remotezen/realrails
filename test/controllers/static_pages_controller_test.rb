11b9bc7d9345cad1410ed66f6cb695b9079197dd
Fred Hudson <remotezen@bell.net>
2015-08-28 09:58:54 -0400

    finished with tutorial part

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,17 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Real Rails Application"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Real Rails Application"
   end
-
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Real Rails Application"
+  end
 end
