a29f94fbc840e4c4e21fa3a844041f3f66b766fb
remotezen <remotezen@bell.net>
2015-09-02 11:25:25 -0400

    first commit for ch08

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,30 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+module Minitest
+  module Reporters
+    class AwesomeReporter < DefaultReporter
+      GREEN = '1;32'
+      RED = '1;31'
+
+      def color_up(string, color)
+        color? ? "\e\[#{ color }m#{ string }#{ ANSI::Code::ENDCODE }" : string
+      end
+
+      def red(string)
+        color_up(string, RED)
+      end
+
+      def green(string)
+        color_up(string, GREEN)
+      end
+    end
+  end
+end
+
+reporter_options = { color: true, slow_count: 5 }
+Minitest::Reporters.use! [Minitest::Reporters::AwesomeReporter.new(reporter_options)]
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
