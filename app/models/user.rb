b77f25df21563ebf85a9987f75166ddab1a63c25
remotezen <remotezen@bell.net>
2015-09-02 09:53:46 -0400

    finished with exercises ch07

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   #EMAIL===========================
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.{1}[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.{1}[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
     format:{ with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false } 
