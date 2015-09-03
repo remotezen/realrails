beb015974ca78fdc4b9148d4bb234221aa8d1a32
remotezen <remotezen@bell.net>
2015-09-03 11:30:14 -0400

    done ch08 on master moving on the exercises

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec  puma -C config/puma.rb -b 'ssl://127.0.0.1:5000?key=/home/grit/.ssh/server.key&cert=/home/grit/.ssh/server.crt'
+web: bundle exec puma -C config/puma.rb -b 'ssl://127.0.0.1:5000?key=/home/grit/.ssh/server.key&cert=/home/grit/.ssh/server.crt'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include Gravtastic
   gravtastic
+  attr_accessor :remember_token
   #NAME========================
   before_save { self.email.downcase!  }
   validates :name, presence: true
@@ -14,6 +15,29 @@ class User < ActiveRecord::Base
 #Password
   has_secure_password
   validates :password, presence: true, length:{ minimum: 6 }
+  def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+  
+  def User.new_token 
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember 
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token) )
+  end
+  def authenticated?(remember_token) 
+    return false if remember_digest.nil? 
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget 
+    update_attribute(:remember_digest, nil)
+  end
 
 end 
 
