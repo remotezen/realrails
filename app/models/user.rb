328b1142126e19d5c99222d0e7c0c0957966568c
remotezen <remotezen@bell.net>
2015-09-10 08:56:32 -0400

    fixed user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
   def activate 
-    update_columns(:activated, true, :activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at:  Time.zone.now)
   end
   
   def send_activation_email 
@@ -35,8 +35,8 @@ class User < ActiveRecord::Base
   end
   def create_reset_digest 
     self.reset_token = User.new_token
-    update_columns(:reset_digest, User.digest(reset_token),
-                   :reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
